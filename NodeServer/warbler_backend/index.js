5931ebbe06489b52d7b4a92149ab51a189a90639
Adam Denoon <adam.denoon@gmail.com>
2018-06-21 09:45:26 -0500

    Add Error Handler to Warbler

--- NodeServer/warbler_backend/index.js
+++ NodeServer/warbler_backend/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const errorHandler = require("./handlers/error")
 
 const PORT = 8080;
 
@@ -15,6 +16,8 @@ app.use((req,res,next) => {
   err.status = 404;
   next(err)
 })
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is starting on port ${PORT}`)
